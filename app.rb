a2b72f7154528e65ef3cefdaa3271782ceced01a
PedroHamzehT <pedrohamzeh@gmail.com>
2022-11-29 23:41:38 -0300

    improve main logic by adding a plateau class

--- app.rb
+++ app.rb
@@ -1,22 +1,20 @@
 # frozen_string_literal: true
 
 require_relative 'lib/rover'
+require_relative 'lib/plateau'
 
 File.open('input.txt', 'r') do |file|
   lines = file.readlines
-  # plateau_limits = lines[0].split
+  plateau = Plateau.new(*lines[0].split)
 
-  rovers = []
   lines[1..].each_slice(2) do |rover_lines|
     rover_position = rover_lines[0].split
     rover = Rover.new(rover_position[0].to_i, rover_position[1].to_i, rover_position[2])
     rover.execute_actions(rover_lines[1].gsub("\n", '').split(''))
 
     p '**********************************'
-    rovers << rover
+    plateau.add_rover(rover)
   end
 
-  rovers.each do |rover|
-    p rover.current
-  end
+  plateau.print_rovers
 end
---
+++ lib/plateau.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+class Plateau
+  def initialize(up_limit, right_limit)
+    @up_limit = up_limit
+    @right_limit = right_limit
+    @rovers = []
+  end
+
+  def add_rover(rover)
+    @rovers << rover
+  end
+
+  def print_rovers
+    @rovers.each do |rover|
+      p rover.current
+    end
+  end
+end
